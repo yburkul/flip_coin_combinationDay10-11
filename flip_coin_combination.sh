42c6be2ea70877284af5a7e9a3763f1a4f39559c
yburkul <yburkul123@gmail.com>
2022-02-25 17:22:11 +0530

    Added: singlet combination

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -4,12 +4,32 @@
 
 tail=0
 head=1
+head_count=0
+tail_count=0
+for(( i=1 ; i<=10 ; i++ ))
+do
+  random_num=$((RANDOM%2))
 
-result=$((RANDOM%2))
-
-if(($result==1))
+if(( $random_num == 1 ))
 then
-    echo "head"
+    singlet[$i]="Head"
+    ((head_count++))
+elif (( $random_num == 0 ))
+then
+    singlet[$i]="Tail"
+    (( tail_count++ ))
 else
-    echo "tail"
+     exit
 fi
+done
+
+#Storing values in dictionary & finding persentage of head & tail
+
+echo ${single[@]}
+
+head_persent=$(( $(($head_count / $i)) * 100 | bc ))
+tail_persent=$(( $(($tail_count / $i)) * 100 | bc ))
+echo "Head count: " $head_count
+echo "Tail count: " $tail_count
+echo "Head persentage in singlet combination:" $head_persent
+echo "Tail persentage in singlet combination:" $tail_persent
